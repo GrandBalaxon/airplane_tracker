d6e632098bb80ba77043ec0e98747aecdb0e2340
Vlad Spinul <shinokakaov@mail.ru>
2026-04-05 19:32:31 +0500

    wrote each and every function from a task to user interface

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import logging
 
 from src.airplane import Airplane
 from src.api import AirplanesAPI
-from src.utils import create_saver, filter_aeroplanes
-
+from src.utils import create_saver, filter_aeroplanes, get_aeroplanes_by_altitude, sort_aeroplanes, get_top_aeroplanes, \
+    print_aeroplanes
 
 logger = logging.getLogger("main")
 
@@ -20,13 +20,13 @@ def user_interaction() -> None:
 
         top_n = int(input("Введите количество самолетов для вывода в топ N по высоте полета: "))
         filter_words = input("Введите названия стран для фильтрации по стране регистрации: ").split()
-        altitude_range = input("Введите диапазон высот полета: ")  # Пример: 100000 - 150000
+        altitude_range = input("Введите диапазон высот полета: ")
 
         filtered_aeroplanes = filter_aeroplanes(airplanes, filter_words)
-        ranged_aeroplanes = get_aeroplanes_by_altitude(airplanes, altitude_range)
-
+        ranged_aeroplanes = get_aeroplanes_by_altitude(filtered_aeroplanes, altitude_range)
         sorted_aeroplanes = sort_aeroplanes(ranged_aeroplanes)
         top_aeroplanes = get_top_aeroplanes(sorted_aeroplanes, top_n)
+
         print_aeroplanes(top_aeroplanes)
 
     else:
@@ -34,9 +34,4 @@ def user_interaction() -> None:
 
 
 if __name__ == '__main__':
-    # Пример работы конструктора класса с одним самолетом
-    aeroplane_1 = Airplane("a612a6", "United States", False,100, 10203.18)
-    aeroplane_2 = Airplane("a612a7", "United States", False, 100, 9203.18)
-
     user_interaction()
-
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from datetime import datetime
 
 from src.airplane import Airplane
@@ -62,6 +63,72 @@ def filter_aeroplanes(airplanes: list[Airplane], filter_words: list[str]) -> lis
             logger.info("Не указано стран для фильтрации списка.")
             return airplanes
 
+    except Exception as e:
+        logger.error(f"Возникла ошибка: {e}")
+        raise
+
+
+def get_aeroplanes_by_altitude(airplanes: list[Airplane], altitude_range: str) -> list[Airplane]:
+    """Функция для фильтрации списка объектов класса Airplane по геометрической высоте.
+
+    Args:
+        airplanes (list[Airplane]): Список объектов класса Airplanes
+        altitude_range (str): Перепад геометрических высок для фильтрации (Пример: 100000 - 150000)
+    """
+    try:
+        if altitude_range:
+            alt_range_cleaned = list(map(int, re.findall(r"\d+", altitude_range)))
+            if len(alt_range_cleaned) == 2:
+                logger.info(f"Высоты для фильтрации: {alt_range_cleaned[0]} - {alt_range_cleaned[1]}.")
+                filtered_list = [
+                    plane for plane in airplanes if alt_range_cleaned[0] <= plane.geo_altitude <= alt_range_cleaned[1]
+                ]
+                return filtered_list
+            else:
+                logger.warning(f"Неверно указан диапазон высот: {alt_range_cleaned[0]}.")
+                return airplanes
+        else:
+            logger.warning(f"Неверно указан диапазон высот: {altitude_range}.")
+            return airplanes
+
+    except Exception as e:
+        logger.error(f"Возникла ошибка: {e}")
+        raise
+
+
+def sort_aeroplanes(airplanes: list[Airplane]) -> list[Airplane]:
+    """Функция для сортировки самолётов по высоте."""
+    try:
+        sorted_aeroplanes = sorted(airplanes, key=lambda x: x.geo_altitude, reverse=True)
+        return sorted_aeroplanes
+
+    except Exception as e:
+        logger.error(f"Возникла ошибка: {e}")
+        return airplanes
+
+
+def get_top_aeroplanes(airplanes: list[Airplane], top_n: int) -> list[Airplane]:
+    """Функция выдаёт N самолётов из списка.
+
+    Args:
+        airplanes (list[Airplane]): Список объектов класса Airplanes
+        top_n (int): Число N - количество выдаваемых функцией позиций самолётов
+    """
+    try:
+        top_list = airplanes[:top_n]
+        return top_list
+
+    except Exception as e:
+        logger.error(f"Возникла ошибка: {e}")
+        return airplanes
+
+
+def print_aeroplanes(airplanes: list[Airplane]) -> None:
+    """Функция для вывода списка самолётов на экран."""
+    try:
+        print("\nФинальный список самолётов:")
+        for airplane in airplanes:
+            print(airplane)
     except Exception as e:
         logger.error(f"Возникла ошибка: {e}")
         raise
